ed37ef3eaa71c23ff6d195834ecc75bb480e1745
wleftwich <wleftwich@gmail.com>
2023-01-26 10:05:56 -0500

    Relative imports in __init__.py

--- phik/__init__.py
+++ phik/__init__.py
@@ -1,15 +1,15 @@
 # flake8: noqa
-from phik.version import version as __version__
+from .version import version as __version__
 
 # pandas dataframe decorators
-from phik import decorators
+from . import decorators
 
 # array functions
-from phik.phik import phik_from_array
-from phik.significance import significance_from_array
-from phik.outliers import outlier_significance_from_array
+from .phik import phik_from_array
+from .significance import significance_from_array
+from .outliers import outlier_significance_from_array
 
 # dataframe functions
-from phik.phik import phik_matrix, global_phik_array
-from phik.significance import significance_matrix
-from phik.outliers import outlier_significance_matrices, outlier_significance_matrix
+from .phik import phik_matrix, global_phik_array
+from .significance import significance_matrix
+from .outliers import outlier_significance_matrices, outlier_significance_matrix
